017a17d493fcac1ec89b299b96d5b4781adfdc32
jhn316 <nissanka_w@yahoo.com>
2024-02-22 18:35:50 +0000

    stream llm output

--- chat.py
+++ chat.py
@@ -4,6 +4,21 @@ llm = AutoModelForCausalLM.from_pretrained(
     "zoltanctoth/orca_mini_3B-GGUF", model_file="orca-mini-3b.q4_0.gguf"
 )
 
-prompt = "Hi! how are you"
+"""
+import torch
+import transformers
 
-print(prompt + llm(prompt))
+branch = "200B"
+llm = transformers.AutoModelForCausalLM.from_pretrained(
+    "LumiOpen/Poro-34B",
+    torch_dtype=torch.bfloat16,
+    revision=branch,
+)
+"""
+
+prompt = "What words complete the sentence The capital of India is"
+
+# print(llm(prompt))
+for word in llm(prompt, stream=True):
+    print(word, end="", flush=True)
+print()
