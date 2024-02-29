5e7f62870240456f7ba5c945df292e2c211ad156
jhn316 <nissanka_w@yahoo.com>
2024-02-29 15:22:09 +0000

    use prompt format

--- chat.py
+++ chat.py
@@ -4,6 +4,29 @@ llm = AutoModelForCausalLM.from_pretrained(
     "zoltanctoth/orca_mini_3B-GGUF", model_file="orca-mini-3b.q4_0.gguf"
 )
 
+
+def get_prompt(instruction: str) -> str:
+    system = "You are an AI assistant that gives helpful answers. You answer the questions in a short and concise way"
+    prompt = f"### System:\n{system}\n\n### User:\n{instruction}\n\n### Response:\n"
+    print(prompt)
+    return prompt
+
+
+question = "Which city is the capital of India"
+
+for word in llm(get_prompt(question), stream=True):
+    print(word, end="", flush=True)
+print()
+
+"""
+prompt = "What words complete the sentence The capital of India is"
+
+# print(llm(prompt))
+for word in llm(prompt, stream=True):
+    print(word, end="", flush=True)
+print()
+"""
+
 """
 import torch
 import transformers
@@ -15,10 +38,3 @@ llm = transformers.AutoModelForCausalLM.from_pretrained(
     revision=branch,
 )
 """
-
-prompt = "What words complete the sentence The capital of India is"
-
-# print(llm(prompt))
-for word in llm(prompt, stream=True):
-    print(word, end="", flush=True)
-print()
